a80781ee09ecd1e832ed9baac3acf58d275a88da
Kae Tzer <kaetzer.tan@ifastfinancial.com>
2022-03-06 22:19:34 +0800

    Navigation: getting the route parameters

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-github-profile',
-  templateUrl: './github-profile.component.html',
-  styleUrls: ['./github-profile.component.css']
+  selector: "app-github-profile",
+  templateUrl: "./github-profile.component.html",
+  styleUrls: ["./github-profile.component.css"],
 })
 export class GithubProfileComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      console.log("params", params.get("username"));
+    });
   }
-
 }
